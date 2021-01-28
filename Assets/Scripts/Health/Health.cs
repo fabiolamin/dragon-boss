fe65b177252308a857bf66a36ac95180f5cec5cd
Fábio Lamin <fabio_lamin@hotmail.com>
2021-01-28 17:11:47 -0300

    Implement life pickup

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -9,15 +9,31 @@ public abstract class Health : MonoBehaviour
     private void Awake()
     {
         _currentHealth = health;
+        UpdateHealthDisplay();
     }
 
     private void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Spell":
+                UpdateHealth(-1f);
+                break;
+            case "Life":
+                UpdateHealth(1f);
+                break;
+        }
+
+        other.gameObject.SetActive(false);
+    }
+
+    protected void GetDamage(Collider other)
     {
         UpdateHealth(-1f);
         other.gameObject.SetActive(false);
     }
 
-    public void UpdateHealth(float amount)
+    protected void UpdateHealth(float amount)
     {
         _currentHealth = Mathf.Clamp(_currentHealth + amount, 0, health);
         UpdateHealthDisplay();
@@ -26,7 +42,7 @@ public abstract class Health : MonoBehaviour
 
     private void CheckHealth()
     {
-        if(_currentHealth <= 0)
+        if (_currentHealth <= 0)
         {
             SetDeath();
         }
