2a4b57d2783d424ce467c4e975ad3de160596440
Fábio Lamin <fabio_lamin@hotmail.com>
2021-02-01 13:50:45 -0300

    Set an event when the Dragon dies

--- Assets/Scripts/Dragon/DragonHealth.cs
+++ Assets/Scripts/Dragon/DragonHealth.cs
@@ -1,13 +1,21 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DragonHealth : Health
 {
+    public delegate void DragonDeath();
+    public static event DragonDeath OnDragonDeath;
+
     [SerializeField] private Text _healthDisplay;
 
     protected override void SetDeath()
     {
-        Debug.Log("Dragon Dead!");
+        OnDragonDeath.Invoke();
+
+        //Increase player's number of arenas
+        //Increase game difficulty
+        //Show the win screen
     }
 
     protected override void UpdateHealthDisplay()
