dedaf768514583f6e0dd98524bdaa4e1099c3029
Fábio Lamin <fabio_lamin@hotmail.com>
2021-02-09 14:59:01 -0300

    Implement the Spell Order

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -4,6 +4,27 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject _storePanel;
+    [SerializeField] private GameObject _playerCoinsDisplay;
+
+    private void Awake()
+    {
+        _storePanel.SetActive(false);
+        _playerCoinsDisplay.SetActive(false);
+    }
+
+    public void ActivateStorePanel()
+    {
+        _storePanel.SetActive(true);
+        _playerCoinsDisplay.SetActive(true);
+    }
+
+    public void ActivateMainMenu()
+    {
+        _storePanel.SetActive(false);
+        _playerCoinsDisplay.SetActive(false);
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
