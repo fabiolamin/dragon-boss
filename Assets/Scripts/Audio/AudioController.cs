e533e4336923e2c954dec2ffc4c4b410a8060db2
Fábio Lamin <fabio_lamin@hotmail.com>
2021-02-10 14:28:44 -0300

    Implement the Audio Controller

---
+++ Assets/Scripts/Audio/AudioController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour
+{
+    private MusicPlayer _musicPlayer;
+    [SerializeField] private Slider _musicSlider, _soundsSlider;
+    [SerializeField] private GameObject _optionsPanel;
+
+    private void Start()
+    {
+        _musicPlayer = FindObjectOfType<MusicPlayer>();
+        _musicPlayer.UpdateVolume(PlayerPrefs.GetFloat("Music"));
+        SetOptionsSliders();
+    }
+
+    private void Update()
+    {
+        if (_optionsPanel.activeSelf)
+        {
+            _musicPlayer.UpdateVolume(_musicSlider.value);
+        }
+    }
+
+    private void SetOptionsSliders()
+    {
+        _musicSlider.value = PlayerPrefs.GetFloat("Music");
+        _soundsSlider.value = PlayerPrefs.GetFloat("Sounds");
+    }
+    public void UpdateAudioVolume()
+    {
+        PlayerPrefs.SetFloat("Music", _musicSlider.value);
+        PlayerPrefs.SetFloat("Sounds", _soundsSlider.value);
+    }
+}
