284967e8b086b829fdc72e43f57534d25d9e5b54
Fábio Lamin <fabio_lamin@hotmail.com>
2021-02-10 15:37:54 -0300

    Implement the button sound

--- Assets/Scripts/UI/ButtonClick.cs
+++ Assets/Scripts/UI/ButtonClick.cs
@@ -5,10 +5,13 @@ using UnityEngine.EventSystems;
 public class ButtonClick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField] private float amountToIncrease = 0.2f;
+    [SerializeField] private AudioClip _buttonAudioClip;
     public void OnPointerDown(PointerEventData eventData)
     {
         transform.localScale +=
         new Vector3(amountToIncrease, amountToIncrease, amountToIncrease);
+
+        AudioSource.PlayClipAtPoint(_buttonAudioClip, Camera.main.transform.position, PlayerPrefs.GetFloat("Sounds"));
     }
 
     public void OnPointerUp(PointerEventData eventData)
