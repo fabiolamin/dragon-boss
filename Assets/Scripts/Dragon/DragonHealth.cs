fa1fd4b8cccd40f05cd16349897f68a86a6ad3e7
Fábio Lamin <fabio_lamin@hotmail.com>
2021-02-17 14:15:27 -0300

    Trigger a dragon's death animation

--- Assets/Scripts/Dragon/DragonHealth.cs
+++ Assets/Scripts/Dragon/DragonHealth.cs
@@ -1,12 +1,26 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class DragonHealth : Health
 {
+    private Animator _animator;
     [SerializeField] private Text _healthDisplay;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     protected override void SetDeath()
     {
+        _animator.SetTrigger("Death");
+        StartCoroutine(DelayDeathTrigger());
+    }
+
+    private IEnumerator DelayDeathTrigger()
+    {
+        yield return new WaitForSeconds(_animator.GetCurrentAnimatorStateInfo(0).length);
         gameObject.SetActive(false);
         DragonController.OnDragonDeath();
     }
