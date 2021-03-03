361dc582fa15623b6c2ca344aa260be5a1601ad9
Fábio Lamin <fabio_lamin@hotmail.com>
2021-03-03 14:50:41 -0300

    Update how the items are spawned

--- Assets/Scripts/Item/ItemSpawner.cs
+++ Assets/Scripts/Item/ItemSpawner.cs
@@ -1,32 +1,58 @@
 using System.Collections;
 using UnityEngine;
+using System.Linq;
 
 public class ItemSpawner : MonoBehaviour
 {
-    [SerializeField] private Item _item;
-    [SerializeField] private Transform[] _spawnLocations;
+    private SpawnLocation[] _spawnLocations;
+
+    [SerializeField] private float _spawnInterval = 1f;
     [SerializeField] private float _minDelay = 4f;
     [SerializeField] private float _maxDelay = 3f;
 
-    public void UpdateItemPosition()
+    private void Awake()
     {
-        int random = Random.Range(0, _spawnLocations.Length);
-        _item.transform.position = new Vector3(_spawnLocations[random].position.x,
-        _item.transform.position.y,
-        _spawnLocations[random].position.z);
-
-        StartCoroutine(SetActivation(false));
+        _spawnLocations = FindObjectsOfType<SpawnLocation>();
     }
 
-    public void SpawnItem()
+    public void StartSpawn(Item item)
     {
-        StartCoroutine(SetActivation(true));
+        StartCoroutine(SetSpawnInterval(item));
     }
 
-    public IEnumerator SetActivation(bool isActive)
+    public IEnumerator SetSpawnInterval(Item item)
     {
+        yield return new WaitForSeconds(_spawnInterval);
+        StartCoroutine(SpawnItem(item));
+    }
+
+    private IEnumerator SpawnItem(Item item)
+    {
+        item.gameObject.SetActive(true);
+        SetItemPosition(item);
         float delay = Random.Range(_minDelay, _maxDelay);
         yield return new WaitForSeconds(delay);
-        _item.gameObject.SetActive(isActive);
+        item.gameObject.SetActive(false);
+    }
+
+    private void SetItemPosition(Item item)
+    {
+        SpawnLocation spawnLocation = GetAvailableSpawnLocation();
+
+        item.transform.position = new Vector3(spawnLocation.transform.position.x,
+        item.transform.position.y,
+       spawnLocation.transform.position.z);
+    }
+
+    private SpawnLocation GetAvailableSpawnLocation()
+    {
+        var availableSpawnLocations = _spawnLocations.Where(s => !s.HasItem);
+        int random = Random.Range(0, availableSpawnLocations.Count());
+        return availableSpawnLocations.ElementAt(random);
+    }
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
     }
 }
