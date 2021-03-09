ad99900d2145ca232ce4bbb550ada77604ec9fe2
Fábio Lamin <fabio_lamin@hotmail.com>
2021-03-09 16:18:46 -0300

    Implement the item spawn vfx

--- Assets/Scripts/Item/SpawnLocation.cs
+++ Assets/Scripts/Item/SpawnLocation.cs
@@ -2,11 +2,24 @@ using UnityEngine;
 
 public class SpawnLocation : MonoBehaviour
 {
+    private ParticleSystem _spawnVFX;
+    [SerializeField] private ParticleSystem _spawnVFXPrefab;
     public bool HasItem { get; set; } = false;
+
+    private void Awake()
+    {
+        _spawnVFX = Instantiate(_spawnVFXPrefab, transform.position + new Vector3(0f, 1f, 0f), Quaternion.identity, transform);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
+        PlaySpawnVFX();
         HasItem = true;
         other.GetComponent<Item>().SpawnLocation = this;
+    }
+
+    public void PlaySpawnVFX()
+    {
+        _spawnVFX.Play();
     }
 }
