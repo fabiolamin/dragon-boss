5108d8df83c13cb0948ef0190149967a6105fcf5
Fábio Lamin <fabio_lamin@hotmail.com>
2021-03-11 15:51:15 -0300

    Include the heroes section in the store panel

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -3,6 +3,7 @@ public class MainMenu : MonoBehaviour
 {
     private AudioController _audioController;
     [SerializeField] private GameObject _storePanel, _optionsPanel;
+    [SerializeField] private GameObject _spellStore, _heroesStore;
     [SerializeField] private GameObject _playerCoinsDisplay;
     [SerializeField] private GameObject _backButton;
 
@@ -11,6 +12,8 @@ public class MainMenu : MonoBehaviour
         _audioController = FindObjectOfType<AudioController>();
         _storePanel.SetActive(false);
         _optionsPanel.SetActive(false);
+        _spellStore.SetActive(true);
+        _heroesStore.SetActive(false);
         _playerCoinsDisplay.SetActive(false);
         _backButton.SetActive(false);
     }
@@ -41,5 +44,17 @@ public class MainMenu : MonoBehaviour
         _playerCoinsDisplay.SetActive(false);
         _backButton.SetActive(false);
         _audioController.UpdateAudioVolume();
+    }
+
+    public void ActivateSpellStore()
+    {
+        _heroesStore.SetActive(false);
+        _spellStore.SetActive(true);
+    }
+
+    public void ActivateHeroesStore()
+    {
+        _heroesStore.SetActive(true);
+        _spellStore.SetActive(false);
     }
 }
