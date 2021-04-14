104d8195dea487364b38bf48931a07a8aaf3f7f0
Fábio Lamin <fabio_lamin@hotmail.com>
2021-04-14 16:24:32 -0300

    Create the Audio Options

---
+++ Assets/Scripts/Audio/AudioOptions.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioOptions : MonoBehaviour
+{
+    [SerializeField] private Slider _musicSlider, _soundsSlider;
+    [SerializeField] private GameObject _optionsPanel;
+    [SerializeField] private float _maxMusicVolume = 0.3f;
+    [SerializeField] private float _maxSoundVolume = 0.8f;
+
+    public float MaxMusicVolume { get { return _maxMusicVolume; } }
+    public float MaxSoundVolume { get { return _maxSoundVolume; } }
+
+    private void Start()
+    {
+        MusicPlayer.Instance.UpdateVolume();
+        SetOptionsSliders();
+    }
+
+    private void Update()
+    {
+        CheckVolume();
+    }
+
+    private void SetOptionsSliders()
+    {
+        _musicSlider.value = PlayerPrefs.GetFloat("Music");
+        _soundsSlider.value = PlayerPrefs.GetFloat("Sound");
+    }
+    private void CheckVolume()
+    {
+        if (_optionsPanel.activeSelf)
+        {
+            PlayerPrefs.SetFloat("Music", Mathf.Clamp(_musicSlider.value, 0f, _maxMusicVolume));
+            PlayerPrefs.SetFloat("Sound", Mathf.Clamp(_soundsSlider.value, 0f, _maxSoundVolume));
+            MusicPlayer.Instance.UpdateVolume();
+        }
+    }
+}
