a28800377da2d9f5e83c26985c17367a3dafb2fb
Fábio Lamin <fabio_lamin@hotmail.com>
2021-04-20 10:01:32 -0300

    Add speed fields in Spell Data

--- Assets/Scripts/Spell/Spell.cs
+++ Assets/Scripts/Spell/Spell.cs
@@ -3,28 +3,27 @@ using UnityEngine;
 public class Spell : MonoBehaviour
 {
     private Rigidbody _spellRb;
+    private float _currentSpeed;
+
     [SerializeField] private SpellData _spellData;
-    [SerializeField] private float _speed = 10f;
-    [SerializeField] private float _maxSpeed = 20f;
 
     public SpellData SpellData { get { return _spellData; } }
-    public float Speed { get { return _speed; } set { _speed = value; } }
-
     public Transform CastingOrigin { get; set; }
 
     private void Awake()
     {
         _spellRb = GetComponent<Rigidbody>();
+        _currentSpeed = _spellData.Speed;
     }
 
     private void OnEnable()
     {
         if (CastingOrigin != null)
-            _spellRb.velocity = CastingOrigin.forward * _speed;
+            _spellRb.velocity = CastingOrigin.forward * _currentSpeed;
     }
 
     public void IncreaseSpeed(float amount)
     {
-        _speed = Mathf.Clamp(_speed + amount, 0, _maxSpeed);
+        _currentSpeed = Mathf.Clamp(_currentSpeed + amount, 0, _spellData.MaxSpeed);
     }
 }
--- Assets/Scripts/Spell/SpellData.cs
+++ Assets/Scripts/Spell/SpellData.cs
@@ -6,8 +6,12 @@ public class SpellData : ScriptableObject
     [SerializeField] private SpellName _spellName;
     [SerializeField] private float _damage = 1f;
     [SerializeField] private int _price = 20;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _maxSpeed;
 
     public SpellName SpellName { get { return _spellName; } }
     public float Damage { get { return _damage; } }
     public int Price { get { return _price; } }
+    public float Speed { get { return _speed; } }
+    public float MaxSpeed { get { return _maxSpeed; } }
 }
