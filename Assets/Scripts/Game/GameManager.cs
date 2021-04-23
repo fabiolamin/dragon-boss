e0eb3632ddfa684d1637a0f876fecdf7c9de4f0b
Fábio Lamin <fabio_lamin@hotmail.com>
2021-04-23 14:42:52 -0300

    Refactor the game manager script

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -3,19 +3,22 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private AudioOptions _audioOptions;
+    [SerializeField] private AudioOptions _audioOptions;
 
     private void Awake()
     {
-        _audioOptions = FindObjectOfType<AudioOptions>();
+        InitializeGame();
+    }
 
+    private void InitializeGame()
+    {
         if (PlayerPrefs.GetInt("FirstPlay") == 0)
         {
             PlayerPrefs.SetFloat("Music", _audioOptions.MaxMusicVolume);
             PlayerPrefs.SetFloat("Sound", _audioOptions.MaxSoundVolume);
             PlayerPrefs.SetInt("Hero", 1);
             PlayerPrefs.SetInt("FirstPlay", 1);
-            PlayerPrefs.SetInt("Coins", 1000);
+            PlayerPrefs.SetInt("Coins", 0);
 
             HeroList heroList = new HeroList();
             heroList.HeroesId = new List<int>() { 1 };
